3e0534b7b436ae3ae0b5a57ad14e3d3e6f781b80
Ming Duan <11203duan@gmail.com>
2019-02-05 02:32:59 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,21 @@
 def my_select(collection)
- # your code here!
-end
+ if collection.empty?
+   return "empty"
+ else
+   i = 0
+   new_array = []
+   while i < collection.length
+     if yield(collection[i]) == true
+       new_array = new_array << collection[i]
+     end
+   i += 1
+ end
+ new_array
+ end
+ end
+
+
+sth = [2,3,4]
+my_select(sth) {
+  |x| x.even?
+}
